8d59fe1cc215cfde33477971c51cb2fb5d717247
hugo lazzari <hugo.luiz@outlook.com>
2019-09-11 13:09:04 -0300

    update life.js

--- life.js
+++ life.js
@@ -3,7 +3,7 @@ const pain = true;
 
 while (existance == true){
     if (existance == pain){
-        console.log('existance is pain');
+        console.log('existance is pain' + ' ' + 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
     }if (existance != pain){
         console.log('this is nice');
     }
