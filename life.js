7f969b138f19f2ef4e43dbe4d935a1ccdde9f973
hugo lazzari <hugo.luiz@outlook.com>
2019-09-11 13:06:21 -0300

    added a little useless js code

---
+++ life.js
@@ -0,0 +1,10 @@
+const existance = true;
+const pain = true;
+
+while (existance == true){
+    if (existance == pain){
+        console.log('existance is pain');
+    }if (existance != pain){
+        console.log('this is nice');
+    }
+}
